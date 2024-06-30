7bcf229d1e8e602db1b927dea218f9516bfd178f
AkhilDeo <singingbird99@gmail.com>
2024-06-30 01:01:59 -0700

    Add profile picture

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,13 @@ function Header() {
   return (
     <header id="header">
       <h1>Akhil Deo</h1>
+      <img src="profile.png" alt="Akhil Deo" />
+      <br></br>
+      <br></br>
       <p>Email: adeo1[at]jhu[dot]edu</p>
-      <p>
-        <a href="https://www.github.com/akhildeo">GitHub</a>
-      </p>
-      <p>
-        <a href="https://www.linkedin.com/in/akhildeo">LinkedIn</a>
-      </p>
+      <a href="https://www.github.com/akhildeo">GitHub</a>
+      <br></br>
+      <a href="https://www.linkedin.com/in/akhildeo">LinkedIn</a>
     </header>
   );
 }
